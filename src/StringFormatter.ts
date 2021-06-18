1639c1f45fa1dc061a6ecd82fb2f55608df066b8
Pon Cheol Ku (구본철) <dev@bbon.kr>
2021-06-18 16:58:20 +0900

    Fix naming convention #10

--- src/StringFormatter.ts
+++ src/StringFormatter.ts
@@ -13,7 +13,7 @@ export class StringFormatter {
      * @returns {string}
      * @memberof StringFormatter
      */
-    public NumberWithDelimiter(
+    public numberWithDelimiter(
         value: number | string,
         delimiter = ',',
     ): string {
@@ -39,5 +39,20 @@ export class StringFormatter {
         stringValue = stringValue.replace(/\B(?=(\d{3})+(?!\d))/g, delimiter);
 
         return `${stringValue}${fraction ? `.${fraction}` : ''}`;
+    }
+
+    /**
+     * 숫자에 1000 단위 구분자를 추갛해서 문자열로 출력합니다.
+     *
+     * @param {(number | string)} value
+     * @param {string} delimiter 구분자; 기본값: **,** 콤마 문자
+     * @returns {string}
+     * @memberof StringFormatter
+     */
+    public NumberWithDelimiter(
+        value: number | string,
+        delimiter = ',',
+    ): string {
+        return this.numberWithDelimiter(value, delimiter);
     }
 }
